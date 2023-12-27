5127ec6435c5ed15815204ea1b7b2946bc274a44
Basbus Alvaro <aebasbus@hotmail.com>
2023-12-27 13:08:34 -0300

    add login form
    add login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { LoginComponent } from './auth/login/login.component';
     FooterComponent,
     HeaderComponent,
     DashboardComponent,
-    LoginComponent
+    LoginComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm=this.formuBuilder.group({
+    usuario:['', [Validators.required]],
+    //email:['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
+
+  constructor(private formuBuilder: FormBuilder, private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+
+  get usuario(){
+    return this.loginForm.controls.usuario;
+  }
+  get password(){
+    return this.loginForm.controls.password;
+  }
+
+
+  login(){
+    if(this.loginForm.valid){
+      console.log('llamar al servicio de login');
+      this.router.navigateByUrl('inicio');
+      this.loginForm.reset;
+    }else{
+      this.loginForm.markAllAsTouched();
+    }
+  }
 
 }
